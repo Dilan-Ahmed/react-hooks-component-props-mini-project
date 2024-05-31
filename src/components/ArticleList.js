b32553c4c230aa58e46ccaa9ad5e45e2d410d018
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-05-31 23:04:09 +0300

    assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      <p>{props.content}</p>
+    </article>
+  );
+}
+
+export default Article;
